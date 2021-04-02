fdc0a329d5f250fdd7ccf59f5a16207ef5a924e6
Pereira-Araujo <araujo_ir@yahoo.com>
2021-04-02 16:34:07 -0300

    Criação: Página de Cadastro

---
+++ src/pages/Register/styles.js
@@ -0,0 +1,24 @@
+
+import styled from "styled-components"
+
+export const Container = styled.div`  
+  width: 100vw;
+  height:80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.p`  
+
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom:1rem;
+  `
+
+export const ContainerForm = styled.form`   
+  display:flex;
+  flex-direction: column;
+`
