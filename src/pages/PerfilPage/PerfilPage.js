242c79f07a5a57600414b62b17bb31cb2451d03a
laecio22 <laecon22@hotmail.com>
2021-04-03 16:13:04 -0300

    Finalização da estilização da página de Perfil

--- src/pages/PerfilPage/PerfilPage.js
+++ src/pages/PerfilPage/PerfilPage.js
@@ -1,29 +1,53 @@
 import React from 'react'
-import{ContainerPerfilPage,ContainerUser,ContainerAddress,ContainerHistoric,CardInfo} from './styles'
+import{
+    ContainerPerfilPage,
+    ContainerUser,
+    ContainerAddress,
+    ContainerHistoric,
+    CardInfo,
+    NameUser,
+    EmailUser,
+    CpfUser,
+    AddressUser,
+    TextHistoric
+} from './styles'
 import EditIcon from '@material-ui/icons/Edit';
+import Divider from '@material-ui/core/Divider';
+import {makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: { 
+        border: 'none',
+        height: '3px',
+        margin: '0 16px',
+        flexShrink: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    },
+});
 
 const PerfilPage = ()=>{
-    
+    const classes = useStyles();
     return(
         <ContainerPerfilPage>          
             <ContainerUser>
                 <CardInfo>
-                    <p>Laécio da Conceição</p>
-                    <p>llll@gmail.com</p>
-                    <p>746.766.766-87</p>
+                    <NameUser>Laécio da Conceição</NameUser>
+                    <EmailUser>llll@gmail.com</EmailUser>
+                    <CpfUser>746.766.766-87</CpfUser>
                 </CardInfo>
                 <EditIcon/>
             </ContainerUser>
             <ContainerAddress>
                 <CardInfo>
                     <p>Endereço cadastrado</p>
-                    <p>Rua almerinda,55 - Ventos Largos </p>
+                    <AddressUser>Rua almerinda,55 - Ventos Largos </AddressUser>
                 </CardInfo>
                 <EditIcon/>
             </ContainerAddress>
-            <ContainerHistoric >                
-                    <p>Histórico de pedidos</p>                    
-                    <div>Você não realizou nenhum pedido</div>                
+            <ContainerHistoric >                     
+                    <TextHistoric>Histórico de pedidos</TextHistoric>                    
+                    <Divider  className={classes.root }/>       
+                    <p>Você não realizou nenhum pedido</p>
             </ContainerHistoric>
         </ContainerPerfilPage>
     )
--- src/pages/PerfilPage/styles.js
+++ src/pages/PerfilPage/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const ContainerPerfilPage = styled.div`
-       
+       font-weight:normal;
 `
 export const ContainerUser = styled.div`
        display:flex;
@@ -13,16 +13,35 @@ export const ContainerAddress = styled.div`
        display:flex;
        justify-content:space-between;
        padding:20px;
-`
-export const ContainerHistoric = styled.div`
-       p{
-           padding:20px;
-       }
 
-       div{
-           text-align:center;
+       p{
+         color:#777;
        }
 `
-export const CardInfo = styled.div`
-    
+export const ContainerHistoric = styled.div`  
+
+       p{
+         text-align:center;
+         padding-top:10px;
+       }       
+`
+export const CardInfo = styled.div`   
+       display:grid;
+       grid-template-rows: 1fr 1fr ;
+       row-gap: 10px; 
+`
+export const NameUser = styled.div`
+
+`
+export const EmailUser = styled.div`
+
+`
+export const CpfUser = styled.div`
+
+`
+export const AddressUser = styled.div`
+       
+`
+export const TextHistoric = styled.div`
+       padding:20px 20px 5px;      
 `
