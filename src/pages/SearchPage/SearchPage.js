60aedc6edebb3627931c8ff7e205928b499d9031
laecio22 <laecon22@hotmail.com>
2021-04-06 03:24:59 -0300

    Criação do Header da página de busca

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import FormPropsTextFields from '../../components/inputBusca/formSearch'
-import {ContainerSearchPage} from './styles'
+import { ContainerSearchPage,SearchPageHeader,Icons,Texto,SubHeaderSearch } from './styles'
+import bluetooth from '../../assets/bluetooth.png'
+import fullBattery from '../../assets/full-battery.png'
+import signal from '../../assets/signal.png'
+import wifi from '../../assets/wifi.png'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
-const SearchPage = ()=>{
-    
-    return(
-        <ContainerSearchPage>              
-            <FormPropsTextFields/> 
-            <p>Busque por nome de restaurante</p>          
+const SearchPage = () => {
+
+    return (
+        <ContainerSearchPage>
+            <SearchPageHeader>
+                <div>
+                    <Icons src={signal} />
+                    <Texto>Sketch</Texto>
+                    <Icons src={wifi} />
+                </div>
+                <Texto>9:41PM</Texto>
+                <div>
+                    <Icons src={bluetooth} />
+                    <Texto>100%</Texto>
+                    <Icons src={fullBattery} />
+                </div>
+            </SearchPageHeader>
+            <SubHeaderSearch>
+                <ArrowBackIosIcon/>
+                <p>Busca</p>
+            </SubHeaderSearch>
+            <FormPropsTextFields />
+            <p>Busque por nome de restaurante</p>
         </ContainerSearchPage>
     )
 }
--- src/pages/SearchPage/styles.js
+++ src/pages/SearchPage/styles.js
@@ -1,7 +1,30 @@
 import styled from 'styled-components'
 
 export const ContainerSearchPage = styled.div`
-       p{
-           text-align:center;
-       }
+        p{
+            text-align:center;
+        }
+`
+export const SearchPageHeader = styled.header`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 5vh;
+        margin: 5px 5px 0 5px;
+        padding: 0 5px 0 5px;
+`
+export const Icons = styled.img`
+        width: 16.5px;
+        height: 16.5px;
+        border-radius: 1px;
+`
+export const Texto = styled.span`
+        margin: 0 5px 0 5px;
+`
+export const SubHeaderSearch = styled.div`
+        margin: 10px 18px;
+        display:grid;
+        grid-template-columns:auto 1fr 1fr;
+        justify-items:flex-end; 
+        column-gap:120px;       
 `
