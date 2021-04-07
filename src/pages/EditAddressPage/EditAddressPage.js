3edec05b85c2a4f2e74a34de3e1e1cf571a8b967
laecio22 <laecon22@hotmail.com>
2021-04-06 21:41:53 -0300

    Criação do Header da página de editar usuário

--- src/pages/EditAddressPage/EditAddressPage.js
+++ src/pages/EditAddressPage/EditAddressPage.js
@@ -1,107 +1,141 @@
 import React from 'react'
 import { useInput } from '../../hooks/useInput'
 import { TextField, Button } from '@material-ui/core'
-import { Title, ContainerForm, Container } from './styles'
+import {
+  ContainerForm,
+  Container,
+  ContainerEditAddressPage,
+  SubHeaderEditAddress,
+  EditAddressPageHeader,
+  Icons,
+  Texto
+
+} from './styles'
+import bluetooth from '../../assets/bluetooth.png'
+import fullBattery from '../../assets/full-battery.png'
+import signal from '../../assets/signal.png'
+import wifi from '../../assets/wifi.png'
+import back from '../../assets/back.png'
+import Divider from '@material-ui/core/Divider';
 
 const EditAddressPage = () => {
-  
 
-    // hook de formulário
-    const { inputText, onChange } = useInput({
-        street: '',
-        number: '',
-        complement: '',
-        neighbourhood: '',
-        city: '',
-        state: '',
-    })
 
-   
-    return (
-        <>
-      
-        <Container>
-          <ContainerForm >
-            <Title>Endereço</Title>
-            <TextField
-              variant="outlined"
-              size="small"
-              label="Logradouro"
-              value={inputText.street}
-              type="text"
-              name="street"
-              placeholder="Rua/Av."
-              onChange={onChange}
-              style={{ margin: '0.5rem 0', width: '18rem' }}
-              required
-            />
-            <TextField
-              variant="outlined"
-              size="small"
-              label="Número"
-              value={inputText.number}
-              type="text"
-              name="number"
-              placeholder="Número"
-              onChange={onChange}
-              style={{ margin: '0.5rem 0' }}
-              required
-            />
-            <TextField
-              variant="outlined"
-              size="small"
-              label="Complemento"
-              value={inputText.complement}
-              type="text"
-              name="complement"
-              placeholder="Apto/Bloco"
-              onChange={onChange}
-              style={{ margin: '0.5rem 0' }}
-            />
-            <TextField
-              variant="outlined"
-              size="small"
-              label="Bairro"
-              value={inputText.neighbourhood}
-              type="text"
-              name="neighbourhood"
-              placeholder="Bairro"
-              onChange={onChange}
-              style={{ margin: '0.5rem 0' }}
-              required
-            />
-            <TextField
-              variant="outlined"
-              size="small"
-              label="Cidade"
-              value={inputText.city}
-              type="text"
-              name="city"
-              placeholder="Cidade"
-              onChange={onChange}
-              style={{ margin: '0.5rem 0' }}
-              required
-            />
-            <TextField
-              variant="outlined"
-              size="small"
-              label="Estado"
-              value={inputText.state}
-              type="text"
-              name="state"
-              placeholder="Estado"
-              onChange={onChange}
-              style={{ margin: '0.5rem 0' }}
-              required
-            />
-            <Button type={'submit'} variant="contained" color="secondary">
-              Salvar
+  // hook de formulário
+  const { inputText, onChange } = useInput({
+    street: '',
+    number: '',
+    complement: '',
+    neighbourhood: '',
+    city: '',
+    state: '',
+  })
+
+
+  return (
+    <>
+      <Container>
+        <ContainerEditAddressPage>
+          <EditAddressPageHeader>
+            <div>
+              <Icons src={signal} />
+              <Texto>Sketch</Texto>
+              <Icons src={wifi} />
+            </div>
+            <Texto>9:41PM</Texto>
+            <div>
+              <Icons src={bluetooth} />
+              <Texto>100%</Texto>
+              <Icons src={fullBattery} />
+            </div>
+          </EditAddressPageHeader>
+          <SubHeaderEditAddress>
+            <Icons src={back} />
+            <p>Editar</p>
+            <div></div>
+          </SubHeaderEditAddress>
+          <Divider />
+        </ContainerEditAddressPage>
+        <ContainerForm >
+          <TextField
+            variant="outlined"
+            size="small"
+            label="Logradouro"
+            value={inputText.street}
+            type="text"
+            name="street"
+            placeholder="Rua/Av."
+            onChange={onChange}
+            style={{ margin: '0.5rem 0', width: '18rem' }}
+            required
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            label="Número"
+            value={inputText.number}
+            type="text"
+            name="number"
+            placeholder="Número"
+            onChange={onChange}
+            style={{ margin: '0.5rem 0' }}
+            required
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            label="Complemento"
+            value={inputText.complement}
+            type="text"
+            name="complement"
+            placeholder="Apto/Bloco"
+            onChange={onChange}
+            style={{ margin: '0.5rem 0' }}
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            label="Bairro"
+            value={inputText.neighbourhood}
+            type="text"
+            name="neighbourhood"
+            placeholder="Bairro"
+            onChange={onChange}
+            style={{ margin: '0.5rem 0' }}
+            required
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            label="Cidade"
+            value={inputText.city}
+            type="text"
+            name="city"
+            placeholder="Cidade"
+            onChange={onChange}
+            style={{ margin: '0.5rem 0' }}
+            required
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            label="Estado"
+            value={inputText.state}
+            type="text"
+            name="state"
+            placeholder="Estado"
+            onChange={onChange}
+            style={{ margin: '0.5rem 0' }}
+            required
+          />
+          <Button type={'submit'} variant="contained" color="secondary">
+            Salvar
             </Button>
-          </ContainerForm>
-        </Container>
-      ) 
+        </ContainerForm>
+      </Container>
+      )
     </>
-    )
+  )
 }
 
 export default EditAddressPage
--- src/pages/EditAddressPage/styles.js
+++ src/pages/EditAddressPage/styles.js
@@ -6,22 +6,43 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
 `
-export const ImageLogo = styled.img`
-  width: 6rem;
-  height: 4rem;
-  margin:1.5rem 8rem 1rem;
- 
+export const ContainerEditAddressPage = styled.div`
+        width: 100vw;
+        p{
+            text-align:center;
+        }
 `
-export const Title = styled.p`
-  font-size: 1rem;
-  font-weight: bold;
-  text-align: center;
-  margin-bottom:1rem;
-  `
-
 export const ContainerForm = styled.form`   
   display:flex;
   flex-direction: column;
+  margin-top:10px;
 `
+export const EditAddressPageHeader = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 5vh;
+        margin: 5px 5px 0 5px;
+        padding: 0 5px 0 5px;
+        flex-wrap:wrap;
+`
+export const Icons = styled.img`
+        width: 16.5px;
+        height: 16.5px;
+        border-radius: 1px;
+`
+export const Texto = styled.span`
+        margin: 0 5px 0 5px;
+`
+export const SubHeaderEditAddress = styled.div`       
+        display:flex;
+        flex-direction: row;
+        justify-content:space-between;
+        margin: 10px 38px 10px 16px;
+        flex-wrap:wrap;      
+       
+`
+
+
