e853e5c2bbe735bebfedc383e698b96b5941df19
laecio22 <laecon22@hotmail.com>
2021-05-01 19:35:22 -0300

    Ajustes dos elementos no card de histórico de pedidos

--- src/components/CardInfoHistoric/CardInfoHistoric.js
+++ src/components/CardInfoHistoric/CardInfoHistoric.js
@@ -10,6 +10,9 @@ import {
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import GlobalStateContext from '../../globalState/globalStateContext';
+import format from 'date-fns/format'
+import { pt } from 'date-fns/locale';
+
 
 const useStyles = makeStyles({
     root: {
@@ -24,16 +27,19 @@ const useStyles = makeStyles({
 const CardInfoHistoric = () => {
     const{states} = useContext(GlobalStateContext)
     const classes = useStyles();
+    
     return (
         <InfoHistoricContainer>
             <TextHistoric>Histórico de pedidos</TextHistoric>
             <Divider className={classes.root} />
             {states.listOrders.length > 0 ? states.listOrders.map((order) => {
+                let data = new Date(order.expiresAt)               
+                let dataFormatada = format(data, 'dd MMMM yyyy', {locale: pt})       
                 return (
                     <CardItemHistoric>
-                        <RestaurantName>{order.name}</RestaurantName>
-                        <DateRequest>{order.date}</DateRequest>
-                        <TotalCost>SUBTOTAL  {order.subtotal}</TotalCost>                       
+                        <RestaurantName>{order.restaurantName}</RestaurantName>
+                        <DateRequest>{dataFormatada}</DateRequest>
+                        <TotalCost>SUBTOTAL  R$ {(order.totalPrice).toFixed(2)}</TotalCost>                       
                     </CardItemHistoric>
                 )
             }) : <p>Você não realizou nenhum pedido</p>}
--- src/components/CardInfoHistoric/styles.js
+++ src/components/CardInfoHistoric/styles.js
@@ -19,23 +19,23 @@ export const CardItemHistoric = styled.div`
        height: 94px;
        border:1px solid rgba(0, 0, 0, 0.25);
        border-radius: 8px;
-       padding: 16px 16px 10px;
+       padding: 16px 16px 4px;
        margin: 7px 16px 0;   
 
 `
 export const RestaurantName = styled.div`
-       margin: 0 0 9px;
+       margin: 0 0 6px;
        font-size: 1rem;
        font-weight: normal;
        color:${green};
 `
 export const DateRequest = styled.div`
-       margin: 9px 0 7px;
+       margin: 5px 0 3px;
        font-size: 0.75rem;
        font-weight: normal;
 `
 export const TotalCost = styled.div`
-       margin: 12px 0 0;
+       margin: 7px 0 3px;
        font-size: 1rem;
        font-weight: bold;
 `
